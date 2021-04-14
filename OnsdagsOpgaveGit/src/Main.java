85eebcd1c8f6567edaa4f50c28ff27d929682203
Silke Marie <silk2918@stud.kea.dk>
2021-04-14 13:51:20 +0200

    modtagKarakterer metode

--- OnsdagsOpgaveGit/src/Main.java
+++ OnsdagsOpgaveGit/src/Main.java
@@ -1,5 +1,32 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+
+
+  private int[] karakterer = new int[7];
+  private ArrayList<Integer> studerende = new ArrayList<>();
+
   public static void main(String[] args) {
-    System.out.println("Hello there");
+    System.out.println("");
+    modtagKarakterer(new int[]{});
+
+  }
+
+  public static int[] modtagKarakterer(int[] karakterer) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Indtast den studerendes karakterer: ");
+
+    for (int i = 1; i < karakterer.length+1; i++) {
+      System.out.println("Indtast karakter nr. " + i );
+      int karakter1 = scan.nextInt();
+      scan.nextLine(); //scanner"bug"
+      int karakter2 = scan.nextInt();
+
+      //if (! equals 3, 4,4, )
+
+    }
+
+    return karakterer;
   }
 }
