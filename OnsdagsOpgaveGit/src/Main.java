0b1a0310ff2b9e644f8ccc703e99f494542bcd56
Kristian Kryger <kristianlaukryger@gmail.com>
2021-04-14 14:53:49 +0200

    Int V2

--- OnsdagsOpgaveGit/src/Main.java
+++ OnsdagsOpgaveGit/src/Main.java
@@ -2,7 +2,8 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-  
+
+
   public static void main(String[] args) {
     System.out.println("Hello there");
     modtagKarakterer(new int[]{});
@@ -11,15 +12,18 @@ public class Main {
 
   public static int[] modtagKarakterer(int[] karakterer) {
     Scanner scan = new Scanner(System.in);
+
     System.out.println("Indtast den studerendes karakterer: ");
 
-    for (int i = 1; i < karakterer.length+1; i++) {
+    int[] karaktererArray = new int[7];
+    int karakter;
+
+    for (int i = 1; i < karaktererArray.length+1; i++) {
       System.out.println("Indtast karakter nr. " + i );
       int karakter1 = scan.nextInt();
       scan.nextLine(); //scanner"bug"
-      int karakter2 = scan.nextInt();
 
-      //if (! equals 3, 4,4, )
+     // if (karakter != -3 || 00 || 2 || 4 || 7 || 10 || 12) {
 
     }
 
