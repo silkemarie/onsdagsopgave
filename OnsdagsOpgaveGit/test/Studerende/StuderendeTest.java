e858339555d71d1ebc963cab324f0c79de5936f5
Kristian Kryger <kristianlaukryger@gmail.com>
2021-04-15 11:27:30 +0200

    Update Studerende og Main

---
+++ OnsdagsOpgaveGit/test/Studerende/StuderendeTest.java
@@ -0,0 +1,13 @@
+package Studerende;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StuderendeTest {
+
+  @Test
+  void testToString() {
+    assertEquals(1, 1);
+  }
+}
